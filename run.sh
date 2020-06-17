c2fe565d11b8af4e1f485f01464abf5635e4f71d
William Nguyen <willhnguyen@gmail.com>
2020-06-17 10:44:11 -0700

    Dockerfile: remove gazebo for ROS gazebo and add ROS init script to .bashrc

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run docker with current directory (pwd) as /workspace
+xhost +local:root # for the lazy and reckless
+docker run --rm -it \
+    --name robond \
+    --privileged \
+    -e DISPLAY=:1 \
+    --gpus all \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -v `pwd`:/workspace \
+    docker.pkg.github.com/willhnguyen/robotics-env/robotics-env:latest \
+    /bin/bash
