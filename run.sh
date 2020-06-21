ed3eddf0e7b962a434c4c130ed407882d3bdc150
William Nguyen <willhnguyen@gmail.com>
2020-06-21 06:49:43 -0700

    Fix display issue with run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ docker run --rm -it \
     --privileged \
     -e DISPLAY=:1 \
     --gpus all \
+    --env="QT_X11_NO_MITSHM=1" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -v `pwd`:/workspace \
     docker.pkg.github.com/willhnguyen/robotics-env/robotics-env:latest \
