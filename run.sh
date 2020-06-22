9ccfe6cde1bdd51c2312eefc1149fc9d3719e047
William Nguyen <willhnguyen@gmail.com>
2020-06-22 15:22:45 -0700

    Add gazebo gui.ini file to avoid default full screen window

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Script Directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Run docker with current directory (pwd) as /workspace
 xhost +local:root # for the lazy and reckless
 docker run --rm -it \
@@ -8,6 +11,7 @@ docker run --rm -it \
     -e DISPLAY=:1 \
     --gpus all \
     --env="QT_X11_NO_MITSHM=1" \
+    -v $DIR/gazebo_gui.ini:/root/.gazebo/gui.ini \
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -v `pwd`:/workspace \
     docker.pkg.github.com/willhnguyen/robotics-env/robotics-env:latest \
